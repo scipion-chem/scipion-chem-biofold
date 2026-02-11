f19149ebbfdf670747d3b0261c9ec18a537d6052
Blanca Pueche <blancapueche@gmail.com>
2026-02-11 14:10:10 +0100

    two separate tests

--- biofold/tests/test_biofold.py
+++ biofold/tests/test_biofold.py
@@ -38,7 +38,7 @@ defSetSeqs = '''1) {"name": "%s", "chain": "%s", "index": "FIRST-LAST", "seqFile
                          (names[0], defSetPDBChain, defSetPDBFile)
 
 
-class TestBiofold(BaseTest):
+class TestChai(BaseTest):
     @classmethod
     def setUpClass(cls):
         cls.ds = DataSet.getDataSet('model_building_tutorial')
@@ -57,6 +57,16 @@ class TestBiofold(BaseTest):
         all = getattr(protChai, 'outputSetOfAtomStructs', None)
         self.assertIsNotNone(all)
 
+    def test(self):
+        self._runChai()
+
+class TestBoltz(BaseTest):
+    @classmethod
+    def setUpClass(cls):
+        cls.ds = DataSet.getDataSet('model_building_tutorial')
+
+        setupTestProject(cls)
+
     def _runBoltz(self):
         protBoltz = self.newProtocol(
             ProtBoltz,
@@ -66,13 +76,12 @@ class TestBiofold(BaseTest):
             samplingSteps=50,
             file=self.ds.getFile('Sequences/3lqd_B_mutated.fasta')
         )
+
         self.launchProtocol(protBoltz)
         best = getattr(protBoltz, 'outputAtomStruct', None)
         self.assertIsNotNone(best)
 
-
     def test(self):
-        self._runChai()
         self._runBoltz()
 
 
